5a123885b50380591cca712a900d9ec6fc6c22eb
Peter Partridge <petertoby@aol.com>
2016-07-13 22:37:50 +0100

    starting the web site
    starting my portfolio website.

---
+++ Portfolio/Scripts/Dialog.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    // hide the hover container
+    $("#hoverText").hide();
+
+    //hover event
+    $("#welcome").hover(function () {
+        $("#hoverText").toggle(1000);
+    });
+
+    //on click function for dialog box
+    $(function () {
+        $("#welcome").click(function () {
+            $("#dialog").dialog("open");
+        });
+    });
+
+
+
+    //dialog function
+    $(function () {
+        $("#dialog").dialog({
+            autoOpen: false,
+            width: 500,
+            height: 500,
+            resizable: false,
+            modal: true,
+            buttons: [{
+                text: "close",
+                click: function () {
+                    $("#dialog").dialog("close");
+                }
+            }
+            ]
+        });
+
+        //hide the title bar
+        $(".ui-dialog-titlebar").hide();
+        $("#dialog").addClass("ui-widget-shadow");
+    });
+});
