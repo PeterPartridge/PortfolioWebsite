6d229e612b2ec4071a6f5cc23bd8723fc6f4529c
Peter Partridge <petertoby@aol.com>
2016-07-25 22:53:34 +0100

    adding scss and carosels

--- Portfolio/Scripts/cycles.js
+++ Portfolio/Scripts/cycles.js
@@ -4,14 +4,26 @@ $("document").ready(function () {
     //javascript framework cycler
     $("#javaFramorks").cycle({
         fx: 'scrollHorz',
-        timeout: 0,
+        timeout: 2000,
         next: '#javaFramorks'
     });
 
     
     $("#programmingLang").cycle({
         fx: 'scrollHorz',
-        timeout: 0,
+        timeout: 2000,
         next: '#programmingLang'
+    });
+
+    $("#ideAndFrameworks").cycle({
+        fx: 'scrollHorz',
+        timeout: 2000,
+        next: '#ideAndFrameworks'
+    });
+    
+    $("#workDone").cycle({
+        fx: 'scrollHorz',
+        timeout: 2000,
+        next: '#workDone'
     });
 });
