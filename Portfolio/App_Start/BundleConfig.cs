bdfa2cf16e9daea6f6d1ed3192a1e3959079bcf9
Peter Partridge <petertoby@aol.com>
2016-08-20 11:57:55 +0100

    making jquery work

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -9,10 +9,7 @@ namespace Portfolio
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js",
-                        "~/Scripts/Dialog.js",
-                        "~/Scripts/visuals.js"));
+                        "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -29,6 +26,21 @@ namespace Portfolio
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                          "~/Content/themes/base/core.css",
+                          "~/Content/themes/base/resizable.css",
+                          "~/Content/themes/base/selectable.css",
+                          "~/Content/themes/base/button.css",
+                          "~/Content/themes/base/dialog.css",
+                          "~/Content/themes/redmond/jquery-ui.redmond.css"));
+
+                bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                                    "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle ("~/bundles/customScripts").Include(
+                         "~/Scripts/Dialog.js",
+                        "~/Scripts/visuals.js"));
         }
     }
 }
--- Portfolio/Scripts/visuals.js
+++ Portfolio/Scripts/visuals.js
@@ -19,68 +19,7 @@
     });
     //when page laods this will fade in and out welcome text and whole document.
 
-    //multiple classes sliding up and down at user click events 
-    //values for drop down boxes
-    var a = 0;
-    var b = 0;
-    var c = 0;
-    var d = 0;
-    //javascript librabry drop down
-    $("#javaLibrary").click(function () {
-        if (a === 0) {
-            $("#angulareJSDropDown").hide();
-            $("#jqueryDropDown").hide();
-            $("#jqueryUiDropDown").hide();
-            $("#javaLibraryDropDown").removeClass("removeClassOnClick").hide().slideDown();
-            a++;
-            
-        }
-        else if (a === 1) {
-            $("#javaLibraryDropDown").slideUp("slow");
-            a--;
-        }
-    });
-
-
-    //programming languages drop down
-    $("#programingLang").click(function () {
-
-        if (b === 0) {
-            $("#programmingLangDropDown").removeClass("removeClassOnClick").hide().slideDown();
-            b++;
-        }
-        else {
-            $("#programmingLangDropDown").slideUp("slow");
-            b--;
-        }
-    });
-
-    //Frameworks drop down
-    $("#framework").click(function () {
-        if (c === 0) {
-            $("#frameworkDropDown").removeClass("removeClassOnClick").hide().slideDown();
-            c++;
-        }
-        else {
-            $("#frameworkDropDown").slideUp("slow");
-            c--;
-        }
-    });
-    //potfolio
-    $("#portfolio").click(function () {
-        if (d === 0) {
-            $("#portfolioDropDown").removeClass("removeClassOnClick").hide().slideDown();
-            d++;
-        }
-        else {
-            $("#portfolioDropDown").slideUp("slow");
-            d--;
-        }
-    });
-    //multiple classes sliding up and down at user click events
-
-
-    //onhover drop down for websites
+    //onhover drop down for websites and personal projects.
     $("#website").hover(function () {
         $("#websiteDropDown").slideToggle({direction:"up"});
     });
